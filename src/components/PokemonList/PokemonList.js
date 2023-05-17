a4386675ef0c357d265b78289c36d739b11444cb
Madi niakate <madi.niakate@accenture.com>
2023-05-17 11:31:56 +0200

    exercice fini

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,12 +1,43 @@
-import React  from "react";
+import React, { useEffect, useState }  from "react";
+import axios from "axios";
 
 function PokemonList({ searchTerm = "" }) {
 
+  const [list, setList] = useState([]);
+  const [listFiltered, setListFiltered]= useState([]);
+  console.log("list",list);
+
+ async function getDatas() {
+    const listPokemons = (await axios.get("https://pokeapi.co/api/v2/pokemon?limit=100")).data.results;
+
+    console.log("resultats : ",listPokemons);
+    setList(listPokemons);
+
+  }
+
+  useEffect(() => {
+    getDatas();
+  },[])
+
+  useEffect(() => {
+   const newList = list.filter((pokemon) => {
+      return pokemon.name.includes(searchTerm);
+    })
+    setListFiltered(newList)
+
+    console.log("filtered",listFiltered);
+  },[searchTerm])
+ 
+
+  const listDisplayed = listFiltered?.length >0 ? listFiltered : list;
 
   return (
     <ul>
-    <li>test</li>
-    <li>test2</li>
+   {
+    listDisplayed.map( (pokemon, index) => {
+      return <li key={index}>{pokemon.name}</li>
+    })
+   }
     </ul>
   );
 }
